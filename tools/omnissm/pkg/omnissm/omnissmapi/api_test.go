c62a66e81051be811d7d66c59d83600d45118416
Kyle Travis <kyle.travis@capitalone.com>
2018-12-13 15:54:09 -0500

    tools/omnissm - [WIP] api tests

--- tools/omnissm/pkg/omnissm/omnissmapi/api_test.go
+++ tools/omnissm/pkg/omnissm/omnissmapi/api_test.go
@@ -27,6 +27,7 @@ import (
 	"github.com/capitalone/cloud-custodian/tools/omnissm/pkg/omnissm"
 	"github.com/google/go-cmp/cmp"
 	"github.com/google/go-cmp/cmp/cmpopts"
+	"github.com/pkg/errors"
 )
 
 var (
@@ -45,6 +46,18 @@ var (
 		),
 	}
 )
+
+func isError(err error) func(error) bool {
+	return func(other error) bool { return err == other }
+}
+
+func isCausedBy(err error) func(error) bool {
+	return func(other error) bool { return err == errors.Cause(other) }
+}
+
+func isDeferredError(e error) bool {
+	return errors.Cause(e) == throttleError && strings.HasPrefix(e.Error(), "deferred action")
+}
 
 // Actual tests
 
@@ -83,16 +96,16 @@ func TestOmniSSMRequestActivation(t *testing.T) {
 	cases := []struct {
 		name          string
 		request       *omnissm.RegistrationRequest
-		entries       map[string]*omnissm.RegistrationEntry
+		registry      registry
 		newActivation func(string) (*ssm.Activation, error)
 		wantDeferred  *DeferredActionMessage
 		wantResponse  *omnissm.RegistrationResponse
 		checkError    func(error) bool
 	}{
 		{
-			name:    "instance already managed",
-			request: req,
-			entries: map[string]*omnissm.RegistrationEntry{managedEntry.Id: managedEntry},
+			name:     "instance already managed",
+			request:  req,
+			registry: newMockRegistry(managedEntry),
 			wantResponse: &omnissm.RegistrationResponse{
 				RegistrationEntry: *managedEntry,
 				Existing:          true,
@@ -100,9 +113,9 @@ func TestOmniSSMRequestActivation(t *testing.T) {
 		},
 
 		{
-			name:    "activiation previously created",
-			request: req,
-			entries: map[string]*omnissm.RegistrationEntry{activationEntry.Id: activationEntry},
+			name:     "activiation previously created",
+			request:  req,
+			registry: newMockRegistry(activationEntry),
 			wantResponse: &omnissm.RegistrationResponse{
 				RegistrationEntry: *activationEntry,
 				Existing:          true,
@@ -112,7 +125,7 @@ func TestOmniSSMRequestActivation(t *testing.T) {
 		{
 			name:          "activiation expired",
 			request:       req,
-			entries:       map[string]*omnissm.RegistrationEntry{oldActivationEntry.Id: oldActivationEntry},
+			registry:      newMockRegistry(oldActivationEntry),
 			newActivation: func(name string) (*ssm.Activation, error) { return &activationEntry.Activation, nil },
 			wantResponse: &omnissm.RegistrationResponse{
 				RegistrationEntry: *activationEntry,
@@ -122,16 +135,33 @@ func TestOmniSSMRequestActivation(t *testing.T) {
 		{
 			name:          "CreateActivation throttled",
 			request:       req,
+			registry:      &errRegistry{get: omnissm.ErrRegistrationNotFound},
 			newActivation: func(name string) (*ssm.Activation, error) { return nil, throttleError },
 			wantDeferred:  &DeferredActionMessage{RequestActivation, req},
-			checkError:    func(e error) bool { return e != nil && strings.HasPrefix(e.Error(), "deferred action") },
+			checkError:    isDeferredError,
 		},
 
 		{
 			name:          "CreateActivation fault",
 			request:       req,
+			registry:      &errRegistry{get: omnissm.ErrRegistrationNotFound},
 			newActivation: func(name string) (*ssm.Activation, error) { return nil, fatalError },
-			checkError:    func(e error) bool { return e == fatalError },
+			checkError:    isError(fatalError),
+		},
+
+		{
+			name:       "registry get error",
+			request:    req,
+			registry:   &errRegistry{get: fatalError},
+			checkError: isError(fatalError),
+		},
+
+		{
+			name:          "registry put error",
+			request:       req,
+			registry:      &errRegistry{get: omnissm.ErrRegistrationNotFound, put: fatalError},
+			newActivation: func(name string) (*ssm.Activation, error) { return &activationEntry.Activation, nil },
+			checkError:    isError(fatalError),
 		},
 	}
 
@@ -139,13 +169,10 @@ func TestOmniSSMRequestActivation(t *testing.T) {
 		t.Run(c.name, func(t *testing.T) {
 			var deferred *DeferredActionMessage
 			o := &OmniSSM{
-				config:   &Config{QueueName: "defer"},
-				registry: &mockRegistry{entries: c.entries},
-				ssmAPI:   &mockSSMAPI{newActivation: c.newActivation},
-				deferQueue: mockQueue(func(msg json.Marshaler) error {
-					deferred = msg.(*DeferredActionMessage)
-					return nil
-				}),
+				config:     &Config{QueueName: "defer"},
+				registry:   c.registry,
+				ssmAPI:     &mockSSMAPI{newActivation: c.newActivation},
+				deferQueue: mockQueue(&deferred),
 			}
 
 			resp, err := o.RequestActivation(context.Background(), c.request)
@@ -164,7 +191,7 @@ func TestOmniSSMRequestActivation(t *testing.T) {
 			}
 			if c.wantResponse != nil {
 				want := &c.wantResponse.RegistrationEntry
-				got := c.entries[want.Id]
+				got := c.registry.(*mockRegistry).entries[want.Id]
 				if d := cmp.Diff(want, got, stdCmpOpts...); d != "" {
 					t.Errorf("unexpected response in registry table: %v", d)
 				}
@@ -175,22 +202,447 @@ func TestOmniSSMRequestActivation(t *testing.T) {
 }
 
 func TestOmniSSMDeregisterInstance(t *testing.T) {
-	//t.Fatal("TODO")
+	type notification struct {
+		Topic string
+		Data  string
+	}
+	newNotification := func(topic string, entry *omnissm.RegistrationEntry) *notification {
+		data, err := json.Marshal(map[string]interface{}{
+			"ManagedId":    entry.ManagedId,
+			"ResourceId":   entry.InstanceId,
+			"AWSAccountId": entry.AccountId,
+			"AWSRegion":    entry.Region,
+		})
+		if err != nil {
+			panic(err)
+		}
+		return &notification{topic, string(data)}
+	}
+
+	existingEntry := &omnissm.RegistrationEntry{
+		Id:        "1",
+		ManagedId: "mi-123",
+		AccountId: "account",
+		Region:    "reg",
+	}
+
+	cases := []struct {
+		name                      string
+		entry                     *omnissm.RegistrationEntry
+		registry                  registry
+		deregisterManagedInstance func(string) error
+		wantDeferred              *DeferredActionMessage
+		wantNotification          *notification
+		checkError                func(error) bool
+	}{
+		{
+			name:     "instance exists",
+			entry:    existingEntry,
+			registry: newMockRegistry(existingEntry),
+			deregisterManagedInstance: func(id string) error {
+				if id != existingEntry.ManagedId {
+					return ssm.ErrInvalidInstance
+				}
+				return nil
+			},
+		},
+		{
+			name:     "instance exists sns",
+			entry:    existingEntry,
+			registry: newMockRegistry(existingEntry),
+			deregisterManagedInstance: func(id string) error {
+				if id != existingEntry.ManagedId {
+					return ssm.ErrInvalidInstance
+				}
+				return nil
+			},
+			wantNotification: newNotification("mytopic", existingEntry),
+		},
+		{
+			name:       "instance does not exist",
+			entry:      existingEntry,
+			registry:   newMockRegistry(),
+			checkError: isCausedBy(omnissm.ErrRegistrationNotFound),
+		},
+		{
+			name:       "instance in registry only",
+			entry:      existingEntry,
+			checkError: isCausedBy(omnissm.ErrRegistrationNotFound),
+			registry:   newMockRegistry(existingEntry),
+
+			// should still receive a notification!
+			wantNotification:          newNotification("mytopic", existingEntry),
+			deregisterManagedInstance: func(id string) error { return ssm.ErrInvalidInstance },
+		},
+		{
+			name:         "ssm throttled",
+			entry:        existingEntry,
+			registry:     newMockRegistry(existingEntry),
+			checkError:   isDeferredError,
+			wantDeferred: &DeferredActionMessage{DeregisterInstance, existingEntry},
+
+			deregisterManagedInstance: func(id string) error { return throttleError },
+		},
+		{
+			name:       "ssm fatal",
+			entry:      existingEntry,
+			registry:   newMockRegistry(existingEntry),
+			checkError: isError(fatalError),
+
+			deregisterManagedInstance: func(id string) error { return fatalError },
+		},
+		{
+			name:         "registry Get throttled",
+			entry:        existingEntry,
+			registry:     &errRegistry{get: throttleError},
+			checkError:   isDeferredError,
+			wantDeferred: &DeferredActionMessage{DeregisterInstance, existingEntry},
+		},
+		{
+			name:       "registry Get fatal",
+			entry:      existingEntry,
+			registry:   &errRegistry{get: fatalError},
+			checkError: isError(fatalError),
+		},
+		{
+			name:       "registry Delete error",
+			entry:      existingEntry,
+			registry:   &errRegistry{delete: throttleError},
+			checkError: isError(throttleError),
+
+			deregisterManagedInstance: func(id string) error { return nil },
+		},
+	}
+
+	for _, c := range cases {
+		t.Run(c.name, func(t *testing.T) {
+			var (
+				deferred         *DeferredActionMessage
+				notified         *notification
+				calledDeregister bool
+			)
+			dereg := c.deregisterManagedInstance
+			if dereg != nil {
+				dereg = func(mid string) error {
+					calledDeregister = true
+					return c.deregisterManagedInstance(mid)
+				}
+			}
+			o := &OmniSSM{
+				config:     &Config{QueueName: "defer"},
+				registry:   c.registry,
+				ssmAPI:     &mockSSMAPI{deregisterManagedInstance: dereg},
+				deferQueue: mockQueue(&deferred),
+				notifier: mockNotifier(func(topic string, data []byte) error {
+					notified = &notification{topic, string(data)}
+					return nil
+				}),
+			}
+			if c.wantNotification != nil {
+				o.config.ResourceDeletedSNSTopic = c.wantNotification.Topic
+			}
+			err := o.DeregisterInstance(context.Background(), c.entry)
+			if err == nil {
+				if _, ok := c.registry.(*mockRegistry).entries[c.entry.Id]; ok {
+					t.Errorf("entry still present in registry: %+v", c.entry)
+				}
+			} else {
+				if c.checkError == nil || !c.checkError(err) {
+					t.Fatalf("unexpected error: %v", err)
+				}
+			}
+			if c.deregisterManagedInstance != nil && !calledDeregister {
+				t.Errorf("DeregisterManagedInstance was not called")
+			}
+
+			if d := cmp.Diff(c.wantDeferred, deferred, stdCmpOpts...); d != "" {
+				t.Errorf("unexpected deferred message: %v", d)
+			}
+
+			if d := cmp.Diff(c.wantNotification, notified, stdCmpOpts...); d != "" {
+				t.Errorf("unexpected response: %v", d)
+			}
+		})
+	}
 }
 
 func TestOmniSSMTagInstance(t *testing.T) {
-	//t.Fatal("TODO")
+	entry := &omnissm.RegistrationEntry{
+		Id:        "1",
+		ManagedId: "mi-123",
+		AccountId: "account",
+		Region:    "reg",
+	}
+
+	normalTags := &ssm.ResourceTags{
+		ManagedId: entry.ManagedId,
+		Tags: map[string]string{
+			"Name": "App",
+		},
+	}
+
+	cases := []struct {
+		name              string
+		tags              *ssm.ResourceTags
+		registry          registry
+		addTagsToResource func(*ssm.ResourceTags) error
+		wantDeferred      *DeferredActionMessage
+		checkError        func(error) bool
+	}{
+		{
+			name:     "tag existing",
+			tags:     normalTags,
+			registry: newMockRegistry(entry),
+
+			addTagsToResource: func(tags *ssm.ResourceTags) error { return nil },
+		},
+		{
+			name:       "tag unkown",
+			tags:       &ssm.ResourceTags{ManagedId: "unknown"},
+			registry:   newMockRegistry(entry),
+			checkError: isCausedBy(omnissm.ErrRegistrationNotFound),
+		},
+		{
+			name:       "registry error",
+			tags:       normalTags,
+			registry:   &errRegistry{getByManagedId: throttleError},
+			checkError: isError(throttleError),
+		},
+		{
+			name:         "ssm throttled",
+			tags:         normalTags,
+			registry:     newMockRegistry(entry),
+			wantDeferred: &DeferredActionMessage{AddTagsToResource, normalTags},
+			checkError:   isDeferredError,
+
+			addTagsToResource: func(tags *ssm.ResourceTags) error { return throttleError },
+		},
+		{
+			name:       "registry tag error",
+			tags:       normalTags,
+			registry:   &errRegistry{entry: entry, setTagged: fatalError},
+			checkError: isError(fatalError),
+
+			addTagsToResource: func(tags *ssm.ResourceTags) error { return nil },
+		},
+	}
+
+	for _, c := range cases {
+		t.Run(c.name, func(t *testing.T) {
+			var (
+				deferred *DeferredActionMessage
+				gotTags  *ssm.ResourceTags
+			)
+			tagResource := c.addTagsToResource
+			if tagResource != nil {
+				tagResource = func(tags *ssm.ResourceTags) error {
+					gotTags = tags
+					return c.addTagsToResource(tags)
+				}
+			}
+			o := &OmniSSM{
+				config:     &Config{QueueName: "defer"},
+				registry:   c.registry,
+				ssmAPI:     &mockSSMAPI{addTagsToResource: tagResource},
+				deferQueue: mockQueue(&deferred),
+			}
+			err := o.TagInstance(context.Background(), c.tags)
+			if err == nil {
+				mr := c.registry.(*mockRegistry)
+				if e, err := mr.GetByManagedId(context.Background(), c.tags.ManagedId); err != nil {
+					t.Errorf("entry for tags not found in registry: %+v", err)
+				} else if e.IsTagged != 1 {
+					t.Errorf("entry is not tagged in registry: %+v", e)
+				}
+
+				if d := cmp.Diff(c.tags, gotTags, stdCmpOpts...); d != "" {
+					t.Errorf("unexpected resource tags: %v", d)
+				}
+			} else if c.checkError == nil || !c.checkError(err) {
+				t.Fatalf("unexpected error: %v", err)
+			}
+
+			if d := cmp.Diff(c.wantDeferred, deferred, stdCmpOpts...); d != "" {
+				t.Errorf("unexpected deferred message: %v", d)
+			}
+		})
+	}
+}
+
+func TestOmniSSMPutInstanceInventory(t *testing.T) {
+	entry := &omnissm.RegistrationEntry{
+		Id:        "1",
+		ManagedId: "mi-123",
+		AccountId: "account",
+		Region:    "reg",
+	}
+
+	inv := &ssm.CustomInventory{
+		TypeName:    "Inventory",
+		ManagedId:   entry.ManagedId,
+		CaptureTime: "2006-01-02T15:04:05Z",
+		Content: map[string]string{
+			"a": "b",
+		},
+	}
+
+	cases := []struct {
+		name         string
+		inv          *ssm.CustomInventory
+		registry     registry
+		putInventory func(*ssm.CustomInventory) error
+		wantDeferred *DeferredActionMessage
+		checkError   func(error) bool
+	}{
+		{
+			name:         "inventory existing",
+			inv:          inv,
+			registry:     newMockRegistry(entry),
+			putInventory: func(tags *ssm.CustomInventory) error { return nil },
+		},
+		{
+			name:       "inventory unknown",
+			inv:        &ssm.CustomInventory{ManagedId: "unknown"},
+			registry:   newMockRegistry(entry),
+			checkError: isCausedBy(omnissm.ErrRegistrationNotFound),
+		},
+		{
+			name:       "registry error",
+			inv:        inv,
+			registry:   &errRegistry{getByManagedId: throttleError},
+			checkError: isError(throttleError),
+		},
+		{
+			name:         "ssm throttled",
+			inv:          inv,
+			registry:     newMockRegistry(entry),
+			putInventory: func(tags *ssm.CustomInventory) error { return throttleError },
+			wantDeferred: &DeferredActionMessage{PutInventory, inv},
+			checkError:   isDeferredError,
+		},
+		{
+			name:         "registry inventory error",
+			inv:          inv,
+			registry:     &errRegistry{entry: entry, setInventoried: fatalError},
+			putInventory: func(tags *ssm.CustomInventory) error { return nil },
+			checkError:   isError(fatalError),
+		},
+	}
+
+	for _, c := range cases {
+		t.Run(c.name, func(t *testing.T) {
+			var (
+				deferred *DeferredActionMessage
+				gotInv   *ssm.CustomInventory
+			)
+			putInventory := c.putInventory
+			if putInventory != nil {
+				putInventory = func(inv *ssm.CustomInventory) error {
+					gotInv = inv
+					return c.putInventory(inv)
+				}
+			}
+			o := &OmniSSM{
+				config:     &Config{QueueName: "defer"},
+				registry:   c.registry,
+				ssmAPI:     &mockSSMAPI{putInventory: putInventory},
+				deferQueue: mockQueue(&deferred),
+			}
+			err := o.PutInstanceInventory(context.Background(), c.inv)
+			if err == nil {
+				mr := c.registry.(*mockRegistry)
+				if e, err := mr.GetByManagedId(context.Background(), c.inv.ManagedId); err != nil {
+					t.Errorf("entry for inventory not found in registry: %+v", err)
+				} else if e.IsInventoried != 1 {
+					t.Errorf("entry is not inventoried in registry: %+v", e)
+				}
+
+				if d := cmp.Diff(c.inv, gotInv, stdCmpOpts...); d != "" {
+					t.Errorf("unexpected instance inventory: %v", d)
+				}
+			} else if c.checkError == nil || !c.checkError(err) {
+				t.Fatalf("unexpected error: %v", err)
+			}
+
+			if d := cmp.Diff(c.wantDeferred, deferred, stdCmpOpts...); d != "" {
+				t.Errorf("unexpected deferred message: %v", d)
+			}
+		})
+	}
 }
 
 func TestOmniSSMTryDefer(t *testing.T) {
-	//t.Fatal("TODO")
+	msg := &DeferredActionMessage{
+		Type:  RequestActivation,
+		Value: "test",
+	}
+	cases := []struct {
+		name         string
+		queueFunc    func() error
+		in           error
+		checkError   func(error) bool
+		wantDeferred *DeferredActionMessage
+	}{
+		{
+			name: "nil error no queue",
+		},
+		{
+			name:      "nil error with queue",
+			queueFunc: func() error { return nil },
+		},
+		{
+			name:       "fatal error",
+			queueFunc:  func() error { return nil },
+			in:         fatalError,
+			checkError: isError(fatalError),
+		},
+		{
+			name:         "throttle error",
+			queueFunc:    func() error { return nil },
+			in:           throttleError,
+			checkError:   isDeferredError,
+			wantDeferred: msg,
+		},
+		{
+			name:         "queue error",
+			queueFunc:    func() error { return fatalError },
+			in:           throttleError,
+			checkError:   isCausedBy(fatalError),
+			wantDeferred: msg,
+		},
+	}
+
+	for _, c := range cases {
+		t.Run(c.name, func(t *testing.T) {
+			var deferred *DeferredActionMessage
+			o := &OmniSSM{config: &Config{QueueName: "defer"}}
+			if c.queueFunc != nil {
+				o.deferQueue = mockQueueFunc(func(x json.Marshaler) error {
+					deferred = x.(*DeferredActionMessage)
+					return c.queueFunc()
+				})
+			}
+			err := o.tryDefer(context.Background(), c.in, msg.Type, msg.Value)
+			if c.checkError != nil {
+				if !c.checkError(err) {
+					t.Fatalf("unexpected error: %v", err)
+				}
+			} else if err != nil {
+				t.Fatalf("unexpected error: %v", err)
+			}
+
+			if d := cmp.Diff(c.wantDeferred, deferred, stdCmpOpts...); d != "" {
+				t.Errorf("unexpected deferred message: %v", d)
+			}
+		})
+	}
 }
 
 // Mocks definitions
 
 var _ ssmAPI = (*mockSSMAPI)(nil)
 var _ notifier = (mockNotifier)(nil)
-var _ deferQueue = (mockQueue)(nil)
+var _ deferQueue = (mockQueueFunc)(nil)
 var _ registry = (*mockRegistry)(nil)
 
 type mockSSMAPI struct {
@@ -219,15 +671,30 @@ func (n mockNotifier) Publish(ctx context.Context, topic string, b []byte) error
 	return n(topic, b)
 }
 
-type mockQueue func(json.Marshaler) error
+type mockQueueFunc func(json.Marshaler) error
 
-func (q mockQueue) Send(ctx context.Context, m json.Marshaler) error {
+func (q mockQueueFunc) Send(ctx context.Context, m json.Marshaler) error {
 	return q(m)
+}
+
+func mockQueue(d **DeferredActionMessage) mockQueueFunc {
+	return func(x json.Marshaler) error {
+		*d = x.(*DeferredActionMessage)
+		return nil
+	}
 }
 
 type mockRegistry struct {
 	mu      sync.RWMutex
 	entries map[string]*omnissm.RegistrationEntry
+}
+
+func newMockRegistry(ee ...*omnissm.RegistrationEntry) *mockRegistry {
+	m := make(map[string]*omnissm.RegistrationEntry)
+	for _, e := range ee {
+		m[e.Id] = e
+	}
+	return &mockRegistry{entries: m}
 }
 
 func (r *mockRegistry) Get(ctx context.Context, id string) (*omnissm.RegistrationEntry, error) {
@@ -296,4 +763,35 @@ func (r *mockRegistry) SetInventoried(ctx context.Context, id string, b bool) er
 		e.IsInventoried = 0
 	}
 	return nil
+}
+
+// mock for returning errors from registry methods
+type errRegistry struct {
+	entry                            *omnissm.RegistrationEntry
+	get, getByManagedId, put, delete error
+	setTagged, setInventoried        error
+}
+
+func (r *errRegistry) Get(ctx context.Context, id string) (*omnissm.RegistrationEntry, error) {
+	return r.entry, r.get
+}
+
+func (r *errRegistry) GetByManagedId(ctx context.Context, mid string) (*omnissm.RegistrationEntry, error) {
+	return r.entry, r.getByManagedId
+}
+
+func (r *errRegistry) Put(ctx context.Context, e *omnissm.RegistrationEntry) error {
+	return r.put
+}
+
+func (r *errRegistry) Delete(ctx context.Context, id string) error {
+	return r.delete
+}
+
+func (r *errRegistry) SetTagged(ctx context.Context, id string, b bool) error {
+	return r.setTagged
+}
+
+func (r *errRegistry) SetInventoried(ctx context.Context, id string, b bool) error {
+	return r.setInventoried
 }
